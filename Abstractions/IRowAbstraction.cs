61fe700bc17f2ad434fe8cf2772b67b4556efbaf
TilliWilli <tilliwilli@gmail.com>
2021-03-19 14:56:34 +0300

    bug/37470 поправил баг с долгим выполнением при наличие условного форматирования

--- Abstractions/IRowAbstraction.cs
+++ Abstractions/IRowAbstraction.cs
@@ -6,7 +6,7 @@ namespace PluginAbstraction
     public interface IRowAbstraction : IDisposable
     {
         IEnumerable<ICellAbstraction> GetCells();
-        IEnumerable<ICellAbstraction> GetUsedCells(bool includeFormats);
+        IEnumerable<ICellAbstraction> GetUsedCells();
         ICellAbstraction GetCell(int index);
         ICellAbstraction FirstCell();
         void InsertRowsBelow(int rowsCount);
--- TemplateCooker/Service/OperationExecutors/CopyPasteRowRangeWithStylesAndFormulas.cs
+++ TemplateCooker/Service/OperationExecutors/CopyPasteRowRangeWithStylesAndFormulas.cs
@@ -36,23 +36,34 @@ namespace TemplateCooking.Service.OperationExecutors
             var to = options.CopyToRow;
             var sheet = workbook.GetSheet(from.SheetIndex);
 
-            int mostRightCellIndex = 0;
+            int mostLeftUsedCellIndex = int.MaxValue; //находим индекс самой левой используемой ячейки (в указаном диапазоне строк которые необходимо скопировать)
+            int mostRightUsedCellIndex = 0; //находим индекс самой правой используемой ячейки (в указаном диапазоне строк которые необходимо скопировать)
             for (var i = from.RowIndex; i <= to.RowIndex; ++i)
             {
-                var columnIndex = sheet.GetRow(i).GetUsedCells(true).LastOrDefault()?.ColumnIndex ?? 0;
-                if (mostRightCellIndex < columnIndex)
-                    mostRightCellIndex = columnIndex;
+                var usedCellsOnRow = sheet.GetRow(i).GetUsedCells();
+                var columnIndex = usedCellsOnRow.LastOrDefault()?.ColumnIndex ?? 0;
+                if (mostRightUsedCellIndex < columnIndex)
+                    mostRightUsedCellIndex = columnIndex;
+
+                var minColumnIndex = usedCellsOnRow.FirstOrDefault()?.ColumnIndex ?? int.MaxValue;
+                if (mostLeftUsedCellIndex > minColumnIndex)
+                    mostLeftUsedCellIndex = minColumnIndex;
             }
 
-            var topLeftCell = sheet.GetRow(from.RowIndex).GetCell(0);
-            var bottomRightCell = sheet.GetRow(to.RowIndex).GetCell(mostRightCellIndex);
+            //если вдруг не нашли ни одной ячейки (что не должно происходить) то навсякий случай выставляем ноль в качестве безопасного значения в такой ситуации
+            if (mostLeftUsedCellIndex == int.MaxValue)
+                mostLeftUsedCellIndex = 0;
+
+            //определяем диапазон и его высоту (тот диапазон который необходимо продублировать (для сохранения оформления ячеек) вниз при смещение строк)
+            var topLeftCell = sheet.GetRow(from.RowIndex).GetCell(mostLeftUsedCellIndex);
+            var bottomRightCell = sheet.GetRow(to.RowIndex).GetCell(mostRightUsedCellIndex);
             var range = sheet.GetRange(topLeftCell, bottomRightCell);
             var rangeHeight = to.RowIndex - from.RowIndex + 1;
 
             for (var i = 0; i < options.PasteCount; ++i)
             {
                 //копируем регион целиком
-                var targetCell = sheet.GetRow(options.PasteStartRow.RowIndex + i * rangeHeight).GetCell(0);
+                var targetCell = sheet.GetRow(options.PasteStartRow.RowIndex + i * rangeHeight).GetCell(mostLeftUsedCellIndex);
                 range.CopyTo(targetCell);
 
                 //удаляем данные во всех ячейках, которые не являються формулами
